03cb7bab44d219462abffe5b4af7a96a8ab053a4
Alexis Guzman <alexis12989x@gmail.com>
2021-01-26 10:51:13 -0600

    Import syles and blur effect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import App from './App';
+
+import './styles/main.scss';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
