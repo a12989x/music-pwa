54b993e45e6f208a8536ab08c605dc5046f1a155
Alexis Guzman <alexis12989x@gmail.com>
2021-01-26 10:50:48 -0600

    Create json file for songs

---
+++ src/audios.js
@@ -0,0 +1,31 @@
+const audios = [
+    {
+        img:
+            'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500',
+        placeholder:
+            'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=80',
+        src: '/assets/cbpd.mp3',
+        title: 'CBPD',
+        artist: 'MXDIK',
+    },
+    {
+        img:
+            'https://images.unsplash.com/photo-1450044804117-534ccd6e6a3a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=500',
+        placeholder:
+            'https://images.unsplash.com/photo-1450044804117-534ccd6e6a3a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=80',
+        src: '/assets/sun-and-his-daughter.mp3',
+        title: 'Sun And His Daughter',
+        artist: 'MXDIK',
+    },
+    {
+        img:
+            'https://images.unsplash.com/photo-1487180144351-b8472da7d491?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=500',
+        placeholder:
+            'https://images.unsplash.com/photo-1487180144351-b8472da7d491?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=80',
+        src: '/assets/kodama-night-town.mp3',
+        title: 'Kodama Night Town',
+        artist: 'MXDIK',
+    },
+];
+
+export default audios;
